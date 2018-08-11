cd7985c54832a3528c82ea7e1128d9db8bed9b38
Emma Bostian <Emma.Bostian@logmein.com>
2018-08-11 09:37:38 +0200

    Adding Recipe component

---
+++ src/components/Recipe.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Recipe extends Component {
+  render() {
+    const ingredients = this.props.ingredients.map((ingredient, i) => <li key={i}>{ingredient}</li>);
+    const steps = this.props.steps.map((step, i) => <li key={i}>{step}</li>);
+    
+    return (
+      <div className="Recipe">
+        <h2>{this.props.title}</h2>
+        <h3>Ingredients</h3>
+        <ul>
+          {ingredients}
+        </ul>
+        <h3>Steps</h3>
+        <ul>
+          {steps}
+        </ul>
+      </div>
+    );
+  }
+}
+
+Recipe.propTypes = {
+  ingredients: PropTypes.array.isRequired,
+  steps: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+export default Recipe;
